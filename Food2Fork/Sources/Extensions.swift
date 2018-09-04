8ffd225180373f52462d43f58d7219172abfde9f
Vadym Markov <markov.vadym@gmail.com>
2018-09-04 16:39:42 +0200

    Add launch screen

---
+++ Food2Fork/Sources/Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  Extensions.swift
+//  Food2Fork
+//
+//  Created by Markov, Vadym on 04/09/2018.
+//  Copyright © 2018 FINN.no AS. All rights reserved.
+//
+
+import Foundation
--- Food2Fork/Sources/ViewController.swift
+++ Food2Fork/Sources/ViewController.swift
@@ -16,4 +16,3 @@ final class ViewController: UIViewController {
         super.viewDidLoad()
     }
 }
-
