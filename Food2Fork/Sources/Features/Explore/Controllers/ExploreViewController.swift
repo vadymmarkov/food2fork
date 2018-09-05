1195559c7713eab294df63d8052649ffa2756190
Vadym <markov.vadym@gmail.com>
2018-09-05 21:25:59 +0200

    Implement explore collection view cell and view controller

--- Food2Fork/Sources/Features/Explore/Controllers/ExploreViewController.swift
+++ Food2Fork/Sources/Features/Explore/Controllers/ExploreViewController.swift
@@ -8,13 +8,79 @@
 
 import UIKit
 
+protocol ExploreViewControllerDelegate: AnyObject {
+    func exploreViewController(_ viewController: ExploreViewController, didSelectRecipe: Recipe)
+}
+
 final class ExploreViewController: UIViewController {
+    weak var delegate: ExploreViewControllerDelegate?
+    private let recipes = [Recipe]()
+    private lazy var collectionView: UICollectionView = self.makeCollectionView()
+
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .default
+    }
 
     // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = R.color.backgroundPrimary()
         navigationItem.title = R.string.localizable.explore()
+
+        view.backgroundColor = R.color.backgroundPrimary()
+        view.addSubview(collectionView)
+        NSLayoutConstraint.pin(collectionView, toView: view)
+
+        collectionView.reloadData()
+    }
+}
+
+// MARK: - UICollectionViewDataSource
+
+extension ExploreViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return recipes.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeue(type: ExploreCollectionViewCell.self, indexPath: indexPath)
+        let recipe = recipes[indexPath.item]
+        return cell
+    }
+}
+
+// MARK: - UICollectionViewDelegate
+
+extension ExploreViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let recipe = recipes[indexPath.item]
+        delegate?.exploreViewController(self, didSelectRecipe: recipe)
+    }
+}
+
+// MARK: - Subviews factory
+
+private extension ExploreViewController {
+    func makeCollectionView() -> UICollectionView {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: makeCollectionViewLayout())
+        collectionView.register(type: ExploreCollectionViewCell.self)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        return collectionView
+    }
+
+    func makeCollectionViewLayout() -> UICollectionViewLayout {
+        let padding: CGFloat = 16
+        let screenWidth = UIScreen.main.bounds.width
+        let itemsPerRow: CGFloat = 2
+        let size = (screenWidth - 4 * padding) / itemsPerRow
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.minimumInteritemSpacing = padding
+        layout.minimumLineSpacing = padding
+        layout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        layout.itemSize = CGSize(width: size, height: size)
+        return layout
     }
 }
--- Food2Fork/Sources/Features/Explore/Views/ExploreCollectionViewCell.swift
+++ Food2Fork/Sources/Features/Explore/Views/ExploreCollectionViewCell.swift
@@ -6,4 +6,32 @@
 //  Copyright © 2018 FINN.no AS. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+final class ExploreCollectionViewCell: UICollectionViewCell {
+    let imageView = UIImageView()
+
+    // MARK: - Init
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(imageView)
+        setupStyles()
+        setupConstraints()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Setup
+
+    private func setupStyles() {
+        contentView.backgroundColor = R.color.backgroundSecondary()
+        contentView.layer.cornerRadius = 10
+    }
+
+    private func setupConstraints() {
+        NSLayoutConstraint.pin(imageView, toView: contentView)
+    }
+}
