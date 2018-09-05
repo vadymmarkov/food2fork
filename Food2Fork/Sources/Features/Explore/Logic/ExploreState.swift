d94c3e54c474abc04c03901f4e1132d02ace2595
Vadym <markov.vadym@gmail.com>
2018-09-06 00:25:13 +0200

    Add mocks

---
+++ Food2Fork/Sources/Features/Explore/Logic/ExploreState.swift
@@ -0,0 +1,9 @@
+//
+//  ExploreState.swift
+//  Food2Fork
+//
+//  Created by Vadym Markov on 05/09/2018.
+//  Copyright © 2018 FINN.no AS. All rights reserved.
+//
+
+import Foundation
---
+++ Food2Fork/Sources/Features/Networking/Data+Decodable.swift
@@ -0,0 +1,9 @@
+//
+//  Data+Decodable.swift
+//  Food2Fork
+//
+//  Created by Vadym Markov on 05/09/2018.
+//  Copyright © 2018 FINN.no AS. All rights reserved.
+//
+
+import Foundation
