f8da9d25463226c324f994f7c9309d4d5c48f0e7
Vadym Markov <markov.vadym@gmail.com>
2018-09-10 15:15:30 +0200

    Add unit tests for App classes

---
+++ Food2ForkTests/Sources/Extensions/NSLayoutConstraintsExtensionsTests.swift
@@ -0,0 +1,9 @@
+//
+//  NSLayoutConstraintsExtensionsTests.swift
+//  Food2ForkTests
+//
+//  Created by Markov, Vadym on 10/09/2018.
+//  Copyright © 2018 FINN.no AS. All rights reserved.
+//
+
+import Foundation
