628fcd06d3a9a0c92497e8ed722e3eee87b72ed7
Vadym Markov <vadym.markov@schibsted.com>
2018-09-12 15:49:48 +0200

    Update APIConfig

---
+++ Food2Fork/Sources/Constants/APIConfig.swift
@@ -0,0 +1,19 @@
+//
+//  APIConfig.swift
+//  Food2Fork
+//
+//  Created by Markov, Vadym on 05/09/2018.
+//  Copyright © 2018 FINN.no AS. All rights reserved.
+//
+
+import Foundation
+
+struct APIConfig {
+    let baseUrl = "https://www.food2fork.com/api"
+    let acceptHeader = "application/json"
+    /**
+     - Sign up for Food2Fork Recipe API: https://www.food2fork.com/about/api
+     - Copy your API key here:
+     */
+    let key = "YOUR_API_KEY"
+}
